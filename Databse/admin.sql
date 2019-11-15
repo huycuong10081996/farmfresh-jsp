ad4f4ba2548883ea6149b5d0e97aba5464ab9c33
nhath <1huynhtrongnhat@gmail.com>
2019-11-15 18:44:28 +0700

    create folder db

---
+++ Databse/admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admin`(`admin_id`, `admin_name`, `admin_password`) VALUES ('AD01', 'TrongNhat', 'admin');
+INSERT INTO `admin`(`admin_id`, `admin_name`, `admin_password`) VALUES ('AD02', 'HuyCuong', 'admin');
+INSERT INTO `admin`(`admin_id`, `admin_name`, `admin_password`) VALUES ('AD03', 'DucTai', 'admin');
---
+++ Databse/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user`(`user_id`, `user_firstName`, `user_lastName`, `user_fullName`, `user_email`, `user_password`) VALUES ('U01', 'Pham  Duc', 'Tai', 'Pham Duc Tai', 'phamductai@gmail.com', '789');
+INSERT INTO `user`(`user_id`, `user_firstName`, `user_lastName`, `user_fullName`, `user_email`, `user_password`) VALUES ('U02', 'Nguyen Huy', 'Cuong', 'Nguyen Huy Cuong', 'huycuong10081996@gmail.com', '456');
+INSERT INTO `user`(`user_id`, `user_firstName`, `user_lastName`, `user_fullName`, `user_email`, `user_password`) VALUES ('U03', 'Huynh Trong', 'Nhat', 'Huynh Trong Nhat', '1huynhtrongnhat@gmail.com', '123');
